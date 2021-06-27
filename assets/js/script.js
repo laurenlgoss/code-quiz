d0e38bc7d6e8c7af020bc2fcacbbfe983e0d430a
Lauren Goss <laurenlgoss98@gmail.com>
2021-06-27 11:44:53 -0400

    pseudo-coded js

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+// Button to start game
+
+
+// Upon button click,
+
+    // Timer begins
+
+    // Question displays
+
+    // Upon answer,
+
+        // Right answer:
+
+            // Displays "correct"
+
+            // Increases user score
+
+        // Wrong answer:
+
+            // Displays "wrong"
+
+            // Decreases user score
+                        
+            // Subtracts time from timer
+
+        // Displays next question
+
+
+// Once timer runs out/game ends
+
+    // Score is totaled and displayed
+
+    // User inputs initials
+
+        // Score/initials are saved
+
+        // Score is displayed in recent scores
+
+    // Game is reset and can be played again
